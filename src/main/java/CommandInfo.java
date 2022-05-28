e2cc162efe11978e1fdf38c4148737107a651354
Jalp P <92553013+jalpp@users.noreply.github.com>
2022-05-28 14:04:16 -0400

    updated commands info

--- src/main/java/CommandInfo.java
+++ src/main/java/CommandInfo.java
@@ -11,12 +11,12 @@ public class CommandInfo {
     }
 
 
-    public EmbedBuilder getCommandInfo(){
+   public EmbedBuilder getCommandInfo(){
 
 
-        this.embedBuilder.setColor(Color.white);
+        this.embedBuilder.setColor(Color.yellow);
         this.embedBuilder.setTitle("Commands for LiSEBot");
-        this.embedBuilder.setDescription("**,help** \n to see command information for the LiSEBot" + "\n\n **,profile <Lichess username>** \n to see lichess profiles for given username" + "\n\n **,top10 <Lichess variant>** \n see the top 10 players list in the variant provided, includes blitz, rapid, classical, bullet, ultrabullet" + "\n\n **,streaming <Lichess username>** Check if your favorite streamer is streaming!" + "\n\n**,team <Team name>** See team information based on team named provided." + "\n\n**,daily** see the daily Lichess puzzle and try to solve it!" + "\n\n **,arena <Lichess arena URL>** see the standings and tournament information for given tournament link" + "\n\n **,stormdash <Lichess username>** see the storm Dashboard of the given user" + "\n\n **,invite** invite LiSEBot to your servers" + "\n\n **,play <variant> <rated/casual> ** created opnended challenge for users to play with, given variant and rated/casual <variant = blitz, rapid, classical, bullet, ultrabullet>" + "\n\n **,engine** <FEN>** get the engine eval and moves for given fen" + "\n\n **,watch** <Lichess Username>** watch latest lichess game of the given user in gif!" + "\n\n **,review** <GameID> ** Get Stockfish game review data for given gameID");
+        this.embedBuilder.setDescription("**/help** \n to see command information for the LiSEBot" + "\n\n **/profile <Lichess username>** \n to see lichess profiles for given username" + "\n\n **/top10 <Lichess variant>** \n see the top 10 players list in the variant provided, includes blitz, rapid, classical, bullet, ultrabullet" + "\n\n **/livestreaming <Lichess username>** Check if your favorite streamer is streaming! \n\n **/streamers** Watch current live streamers "  + "\n\n**/team <Team name>** See team information based on team named provided." + "\n\n**/daily** see the daily Lichess puzzle and try to solve it!" + "\n\n **/arena <Lichess arena URL>** see the standings and tournament information for given tournament link" + "\n\n **/stormdash <Lichess username>** see the storm Dashboard of the given user" + "\n\n **/invite** invite LiSEBot to your servers" + "\n\n **/play <variant> <rated/casual> ** created opnended challenge for users to play with, given variant and rated/casual <variant = blitz, rapid, etc>" + "\n\n **/watch** <Lichess Username>** watch latest lichess game of the given user in gif!" + "\n\n **/gamereview** <GameID>** Get Stockfish game review data for given gameID" + "\n\n ** Lichess Auth Commands** \n\n **/challengeauth <Lichess Personal Token> <Opponent username>** challenge anyone directly from Discord \n\n **/chatauth <Lichess Personal Token> <message Receiver username> <message content>** Message a Lichess user from discord! \n\n **/scheduletournament <Lichess Personal Token> <Tournament variant (blitz, etc)>");
 
 
         return this.embedBuilder;
