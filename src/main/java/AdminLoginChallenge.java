49b7e5e9e152571dc232f43a5b8185bb9abecddf
Jalp P <92553013+jalpp@users.noreply.github.com>
2022-05-31 14:25:44 -0400

    refactored subclass of AdminActionsManager class

--- src/main/java/AdminLoginChallenge.java
+++ src/main/java/AdminLoginChallenge.java
@@ -1,47 +1,35 @@
 
 import chariot.Client;
 import chariot.ClientAuth;
-import chariot.api.ChallengesAuth;
-import chariot.model.Arena;
-import chariot.model.BulkPairing;
+
 import chariot.model.Result;
 import chariot.model.User;
 import net.dv8tion.jda.api.EmbedBuilder;
-
 import java.awt.*;
-import java.time.*;
-import java.util.function.Consumer;
 
 
-public class AdminLoginChallenge {
+public class AdminLoginChallenge extends AdminActionsManager{
 
-
- private String AdminToken;
- private String userID;
  private EmbedBuilder embedBuilder;
- private Client basicClient;
 
-
- public AdminLoginChallenge(Client client, String LichessToken, String userID){
-  this.basicClient = client;
-  this.AdminToken = LichessToken;
-  this.userID = userID;
-
+ public AdminLoginChallenge(Client client, String LichessToken, String userID) {
+  super(client, LichessToken, userID);
  }
 
  public EmbedBuilder getChallenge(){
   this.embedBuilder = new EmbedBuilder();
-  ClientAuth clientAuth = Client.auth(this.AdminToken);
+  ClientAuth clientAuth = Client.auth(this.getLichessToken());
 
-  Result<User> userResult = this.basicClient.users().byId(this.userID, true);
+  Result<User> userResult = this.getBasicClient().users().byId(this.getUserID(), true);
 
   boolean userPresent = userResult.isPresent();
 
+
   if(userPresent){
-   clientAuth.challenges().challenge(this.userID, conf -> conf.clock(300, 3).color(c -> c.white()));
+   String url = clientAuth.challenges().challenge(this.getUserID(), conf -> conf.clock(300, 3).color(c -> c.white())).get().url();
    this.embedBuilder.setColor(Color.green);
-   this.embedBuilder.setTitle("Successfully Created Challenge against: " + this.userID);
-   this.embedBuilder.setDescription("Please Start your game here: " + "[Play Here](https://lichess.org/@/" + this.userID +")");
+   this.embedBuilder.setTitle("Successfully Created Challenge against: " + this.getUserID());
+   this.embedBuilder.setDescription("Please Start your game here: " + "[Play Here](" + url + ")");
    return this.embedBuilder;
   }else{
 
@@ -58,4 +46,5 @@ public class AdminLoginChallenge {
 }
 
 
-}
+
+ }
