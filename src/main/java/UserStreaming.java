450bf72ab58c564185f6e55b214689f6dbfdfe31
jalpp <92553013+jalpp@users.noreply.github.com>
2022-06-09 13:00:24 -0400

    fixed some bugs with inactive streamers

--- src/main/java/UserStreaming.java
+++ src/main/java/UserStreaming.java
@@ -13,7 +13,7 @@ public class UserStreaming extends UserProfile{
 
 
 
-    public UserStreaming(Client client, String[] parsingUser){
+    public UserStreaming(Client client, String parsingUser){
         super(client, parsingUser);
 
 
@@ -34,47 +34,61 @@ public class UserStreaming extends UserProfile{
 
     public EmbedBuilder getStreamingStatus(){
 
-        Result<User> result = this.getClient().users().byId(this.getUserID());
-        this.embedBuilder = new EmbedBuilder();
+        try {
 
-        if (result.isPresent()) { // checking if the user is present
+            Result<User> result = this.getClient().users().byId(this.getUserID());
+            this.embedBuilder = new EmbedBuilder();
 
-            User user = result.get();
+            if (result.isPresent()) { // checking if the user is present
 
-            String sayTitle = "";
+                User user = result.get();
 
-            if(user.title().isPresent()){
-                sayTitle += user.title().get();
-            }else{
-                sayTitle += "";
+                String sayTitle = "";
+
+                if (user.title().isPresent()) {
+                    sayTitle += user.title().get();
+                } else {
+                    sayTitle += "";
+                }
+
+
+                if (user.tosViolation() == true) { // check to see if the user is a cheater
+                    this.embedBuilder.setDescription(this.getUserID() + "  has violated Lichess terms of service ");
+                }
+
+                if (user.closed() == true) { // check to see if the user is a closed account
+                    this.embedBuilder.setDescription(this.getUserID() + " is a closed account");
+                }
+
+                if (result.get().count().all() > 20) {
+
+                    if (user.streaming() == true && user.tosViolation() == false && user.closed() == false) { // displaying the streaming status
+
+                        this.embedBuilder.setColor(Color.green);
+                        String stream = user.url();
+
+                        this.embedBuilder.setDescription(sayTitle + " " + this.getUserID() + " Is Streaming! " + "✅" + " View the Stream [Here in the Lichess profile](" + stream + ")");
+
+                    }
+                    if (user.streaming() == false && user.tosViolation() == false && user.closed() == false) { // displaying the non-stream status
+                        this.embedBuilder.setColor(Color.red);
+                        this.embedBuilder.setDescription(sayTitle + " " + this.getUserID() + " Is not Streaming " + "❌" + " Check back later!");
+
+                    }
+                } else {
+                    this.embedBuilder = new EmbedBuilder();
+                    return this.embedBuilder.setDescription("User is not active please play more games to live-stream");
+                }
+
+
+            } else {
+                this.embedBuilder.setDescription("User not present, Please try again");
             }
 
-
-            if (user.tosViolation() == true) { // check to see if the user is a cheater
-               this.embedBuilder.setDescription(this.getUserID() + "  has violated Lichess terms of service ");
-            }
-
-            if (user.closed() == true) { // check to see if the user is a closed account
-               this.embedBuilder.setDescription(this.getUserID() + " is a closed account");
-            }
-
-            if (user.streaming() == true && user.tosViolation() == false && user.closed() == false) { // displaying the streaming status
-
-                this.embedBuilder.setColor(Color.green);
-                String stream = user.url();
-
-                this.embedBuilder.setDescription(sayTitle + " " + this.getUserID() + " Is Streaming! " + "✅" + " View the Stream [Here in the Lichess profile](" + stream + ")");
-
-            }
-            if (user.streaming() == false && user.tosViolation() == false && user.closed() == false) { // displaying the non-stream status
-                this.embedBuilder.setColor(Color.red);
-                this.embedBuilder.setDescription(sayTitle+ " " + this.getUserID() + " Is not Streaming " + "❌" + " Check back later!");
-
-            }
-
-
-        } else {
-           this.embedBuilder.setDescription("User not present, Please try again");
+        }catch(Exception e){
+            e.printStackTrace();
+            this.embedBuilder = new EmbedBuilder();
+            return this.embedBuilder.setDescription("Error occurred please try again");
         }
 
         return this.embedBuilder;
