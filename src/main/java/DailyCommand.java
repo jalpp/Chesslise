d6f90d4f5d76a02cd6f50e08761e86384b7d2eff
jalpp <92553013+jalpp@users.noreply.github.com>
2022-06-12 21:11:25 -0400

    added puzzle images

--- src/main/java/DailyCommand.java
+++ src/main/java/DailyCommand.java
@@ -1,4 +1,5 @@
 import chariot.Client;
+import chariot.model.ExploreResult;
 import chariot.model.Puzzle;
 import chariot.model.Result;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -8,52 +9,61 @@ import java.util.List;
 
 public class DailyCommand {
 
-   private Client client;
-   private EmbedBuilder embedBuilder;
+    private Client client;
+    private EmbedBuilder embedBuilder;
 
     public DailyCommand(Client client) {
         this.client = client;
     }
 
-
-    /**
-     *
-     * ,daily command to see the daily puzzles of lichess [I LOVE THEM]
-     *
-     * input: None
-     *
-     * output: the puzzle rating and the themes the puzzle is about, you can try the puzzle on lichess also
-     *
-     *
-     *
-     */
-
-
+    
 
     public EmbedBuilder getPuzzleData() {
-        Result<Puzzle> dailypuzzle = client.puzzles().dailyPuzzle();
 
-        if (dailypuzzle.isPresent()) { // check if the puzzle is present
-            Puzzle puzzle1 = dailypuzzle.get();
+        try {
 
-            Puzzle.PuzzleInfo puzzleInfo = puzzle1.puzzle();
+            Result<Puzzle> dailypuzzle = client.puzzles().dailyPuzzle();
 
-            String url = "https://lichess.org/training/daily";
 
-            int puzzleRating = puzzleInfo.rating();
-            List<String> themes = puzzleInfo.themes();
+            if (dailypuzzle.isPresent()) { // check if the puzzle is present
+                Puzzle puzzle1 = dailypuzzle.get();
 
-            String the = "";
 
-            for (int i = 0; i < themes.size(); i++) {
-                the += themes.get(i) + " \n ";
+                String puzzleID = puzzle1.puzzle().id();
+                String puzzleImg = "https://lichess1.org/training/export/gif/thumbnail/"  + puzzleID + ".gif";
+
+                Puzzle.PuzzleInfo puzzleInfo = puzzle1.puzzle();
+
+                String url = "https://lichess.org/training/daily";
+
+                int puzzleRating = puzzleInfo.rating();
+                List<String> themes = puzzleInfo.themes();
+
+                String the = "";
+
+                for (int i = 0; i < themes.size(); i++) {
+                    the += themes.get(i) + " \n ";
+                }
+
+                this.embedBuilder = new EmbedBuilder();
+                this.embedBuilder.setColor(Color.white);
+                this.embedBuilder.setTitle("Daily Puzzle");
+                this.embedBuilder.setImage(puzzleImg);
+                this.embedBuilder.setDescription("**Puzzle rating:** \n" + puzzleRating + "\n\n **Puzzle Themes:** \n " + the + "\n [Try the Puzzle](" + url + ")");
+
+              return this.embedBuilder;
             }
 
-            this.embedBuilder = new EmbedBuilder();
-            this.embedBuilder.setColor(Color.white);
-            this.embedBuilder.setTitle("Daily Puzzle");
-            this.embedBuilder.setDescription("**Puzzle rating:** \n" + puzzleRating + "\n\n **Puzzle Themes:** \n " + the + "\n [Try the Puzzle](" + url + ")");
 
+
+        } catch (ExceptionInInitializerError ex) {
+
+            Throwable throwable = ex.getException();
+            if (throwable != null) {
+                throwable.printStackTrace();
+            } else {
+                System.out.println("Throwable not present");
+            }
 
 
         }
@@ -63,4 +73,9 @@ public class DailyCommand {
     }
 
 
+
+
+
 }
+
+
