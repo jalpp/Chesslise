b5b36d2083e3c4fd42a4be8df2609ff444343a5e
jalpp <92553013+jalpp@users.noreply.github.com>
2022-06-18 15:47:42 -0400

    added Lichess blogs object

---
+++ src/main/java/Blog.java
@@ -0,0 +1,34 @@
+public class Blog {
+
+    private String name;
+    private String topic;
+    private String url;
+    private int year;
+
+
+    public Blog(String topic){
+        this.topic = topic;
+    }
+
+    public Blog(int year){
+        this.year = year;
+    }
+
+    public Blog(String name, String topic, String url){
+        this.name = name;
+        this.topic = topic;
+        this.url = url;
+    }
+
+    public String getLatestBlog(){
+        return "https://lichess.org/blog";
+    }
+
+    public String getBlogsByTopic(){
+        return "https://lichess.org/blog/topic/" + this.topic;
+    }
+
+
+
+
+}
