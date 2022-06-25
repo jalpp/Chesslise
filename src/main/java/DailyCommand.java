a79e003db904b92e96a4e233467a218a40a94003
jalpp <92553013+jalpp@users.noreply.github.com>
2022-06-25 16:22:53 -0400

    added watching method

--- src/main/java/DailyCommand.java
+++ src/main/java/DailyCommand.java
@@ -2,6 +2,8 @@ import chariot.Client;
 import chariot.model.ExploreResult;
 import chariot.model.Puzzle;
 import chariot.model.Result;
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.move.MoveList;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import java.awt.*;
@@ -11,12 +13,14 @@ public class DailyCommand {
 
     private Client client;
     private EmbedBuilder embedBuilder;
+    private String gameID;
 
     public DailyCommand(Client client) {
         this.client = client;
+        this.gameID = "";
     }
 
-    
+
 
     public EmbedBuilder getPuzzleData() {
 
@@ -28,9 +32,22 @@ public class DailyCommand {
             if (dailypuzzle.isPresent()) { // check if the puzzle is present
                 Puzzle puzzle1 = dailypuzzle.get();
 
+                this.gameID = puzzle1.game().id();
+
+
 
                 String puzzleID = puzzle1.puzzle().id();
                 String puzzleImg = "https://lichess1.org/training/export/gif/thumbnail/"  + puzzleID + ".gif";
+
+                List<String> solution = puzzle1.puzzle().solution();
+
+                String textMoves = "";
+
+                for(int i = 0; i < solution.size(); i++){
+                    textMoves += solution.get(i) + " ";
+                }
+
+
 
                 Puzzle.PuzzleInfo puzzleInfo = puzzle1.puzzle();
 
@@ -46,8 +63,9 @@ public class DailyCommand {
                 }
 
                 this.embedBuilder = new EmbedBuilder();
-                this.embedBuilder.setColor(Color.white);
+                this.embedBuilder.setColor(Color.blue);
                 this.embedBuilder.setTitle("Daily Puzzle");
+                this.embedBuilder.setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/155/jigsaw-puzzle-piece_1f9e9.png");
                 this.embedBuilder.setImage(puzzleImg);
                 this.embedBuilder.setDescription("**Puzzle rating:** \n" + puzzleRating + "\n\n **Puzzle Themes:** \n " + the + "\n [Try the Puzzle](" + url + ")");
 
@@ -70,6 +88,16 @@ public class DailyCommand {
 
         return this.embedBuilder;
 
+    }
+
+
+    public String getPuzzleGame(){
+        Result<Puzzle> dailypuzzle = client.puzzles().dailyPuzzle();
+        this.gameID = dailypuzzle.get().game().id();
+        String puzzleGame = "";
+        puzzleGame += "https://lichess1.org/game/export/gif/" + this.gameID + ".gif";
+
+        return puzzleGame;
     }
 
 
