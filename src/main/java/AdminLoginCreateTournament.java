3ace2430f8d2efbaffe12dd38bfa62fbb4c8dc1d
jalpp <92553013+jalpp@users.noreply.github.com>
2022-06-29 15:37:50 -0400

    added delete token, catch some errors

--- src/main/java/AdminLoginCreateTournament.java
+++ src/main/java/AdminLoginCreateTournament.java
@@ -1,10 +1,12 @@
+import chariot.Client;
+import chariot.ClientAuth;
+import chariot.model.Ack;
+import chariot.model.Result;
 import net.dv8tion.jda.api.EmbedBuilder;
-
 import java.awt.*;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class AdminLoginCreateTournament{
 
@@ -21,35 +23,48 @@ public class AdminLoginCreateTournament{
 
     public EmbedBuilder getCreatedTournament(){
 
-
-        this.embedBuilder = new EmbedBuilder();
-        this.embedBuilder.setColor(Color.orange);
-        this.embedBuilder.setTitle("Tournament Created!");
-        this.embedBuilder.setDescription("Tournament created! Please note, tournament time, name, description are pre set, change it according from your Lichess account!");
-        var tomorrow = ZonedDateTime.now().plusDays(3).with(
-                LocalTime.parse("17:00"));
-
-
         ClientAuth clientAuth = Client.auth(this.adminToken);
 
+        try {
 
-        switch(this.timecontrol) {
-            case "blitz":
-                clientAuth.tournaments().createArena(params -> params.clockBlitz3m1s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
-                break;
-            case "rapid":
-                clientAuth.tournaments().createArena(params -> params.clockRapid10m0s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
-                break;
-            case "classical":
-                clientAuth.tournaments().createArena(params -> params.clockClassical30m0s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
-                break;
-            case "bullet":
-                clientAuth.tournaments().createArena(params -> params.clockBullet1m0s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
-                break;
-            default:
-               return this.embedBuilder.setDescription("error occured, please input a proper variant: supported varaints are: blitz,rapid,classical,bullet");
+
+
+                this.embedBuilder = new EmbedBuilder();
+                this.embedBuilder.setColor(Color.orange);
+                this.embedBuilder.setTitle("Tournament Created!");
+                this.embedBuilder.setDescription("Tournament created! Please note, tournament time, name, description are pre set, change it according from your Lichess account!");
+                var tomorrow = ZonedDateTime.now().plusDays(3).with(
+                        LocalTime.parse("17:00"));
+
+
+                switch (this.timecontrol) {
+                    case "blitz":
+                        clientAuth.tournaments().createArena(params -> params.clockBlitz3m1s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
+
+                        break;
+                    case "rapid":
+                        clientAuth.tournaments().createArena(params -> params.clockRapid10m0s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
+
+                        break;
+                    case "classical":
+                        clientAuth.tournaments().createArena(params -> params.clockClassical30m0s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
+                        break;
+                    case "bullet":
+                        clientAuth.tournaments().createArena(params -> params.clockBullet1m0s().name("my tournament").description("tournament made by LISEBOT").startTime(tomorrow));
+                        break;
+                    default:
+                        return this.embedBuilder.setDescription("error occured, please input a proper variant: supported varaints are: blitz,rapid,classical,bullet");
+
+                }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            this.embedBuilder = new EmbedBuilder();
+            Result<Ack> deleteToken = clientAuth.account().revokeToken();
+            return this.embedBuilder.setDescription("Error Occurred, Please provide proper input!");
         }
 
+        Result<Ack> deleteToken = clientAuth.account().revokeToken();
         return this.embedBuilder;
 
     }
