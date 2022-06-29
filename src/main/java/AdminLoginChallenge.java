a4573e9feead119bcf64c22206c4ab272fb2aacc
jalpp <92553013+jalpp@users.noreply.github.com>
2022-06-29 15:36:12 -0400

    added delete client token for safer bot use

--- src/main/java/AdminLoginChallenge.java
+++ src/main/java/AdminLoginChallenge.java
@@ -2,6 +2,7 @@
 import chariot.Client;
 import chariot.ClientAuth;
 
+import chariot.model.Ack;
 import chariot.model.Result;
 import chariot.model.User;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -17,27 +18,35 @@ public class AdminLoginChallenge extends AdminActionsManager{
  }
 
  public EmbedBuilder getChallenge(){
-  this.embedBuilder = new EmbedBuilder();
   ClientAuth clientAuth = Client.auth(this.getLichessToken());
+  try {
+   this.embedBuilder = new EmbedBuilder();
 
-  Result<User> userResult = this.getBasicClient().users().byId(this.getUserID(), true);
+   Result<User> userResult = this.getBasicClient().users().byId(this.getUserID(), true);
 
-  boolean userPresent = userResult.isPresent();
+   boolean userPresent = userResult.isPresent();
 
 
-  if(userPresent){
-   String url = clientAuth.challenges().challenge(this.getUserID(), conf -> conf.clock(300, 3).color(c -> c.white())).get().url();
-   this.embedBuilder.setColor(Color.green);
-   this.embedBuilder.setTitle("Successfully Created Challenge against: " + this.getUserID());
-   this.embedBuilder.setDescription("Please Start your game here: " + "[Play Here](" + url + ")");
-   return this.embedBuilder;
-  }else{
+   if (userPresent && !userResult.get().closed() && !userResult.get().tosViolation()) {
+    String url = clientAuth.challenges().challenge(this.getUserID(), conf -> conf.clock(300, 3).color(c -> c.white())).get().url();
+    this.embedBuilder.setColor(Color.green);
+    this.embedBuilder.setTitle("Successfully Created Challenge against: " + this.getUserID());
+    this.embedBuilder.setDescription("Please Start your game here: " + "[Play Here](" + url + ")");
+    Result<Ack> deleteToken = clientAuth.account().revokeToken();
+    return this.embedBuilder;
+   } else {
+    this.embedBuilder.setColor(Color.red);
+    this.embedBuilder.setTitle("Challenge Failed!");
+    this.embedBuilder.setDescription("Invalid Input, Please input proper Lichess username!");
+    Result<Ack> deleteToken = clientAuth.account().revokeToken();
+    return this.embedBuilder;
+   }
 
-   this.embedBuilder.setColor(Color.red);
-   this.embedBuilder.setTitle("Challenge Failed!");
-   this.embedBuilder.setDescription("Invalid Input, Please input proper Lichess username!");
 
-   return this.embedBuilder;
+  }catch (Exception e){
+   this.embedBuilder = new EmbedBuilder();
+   Result<Ack> deleteToken = clientAuth.account().revokeToken();
+   return this.embedBuilder.setDescription("Error Occurred, please provide proper input!");
   }
 
 
@@ -47,4 +56,8 @@ public class AdminLoginChallenge extends AdminActionsManager{
 
 
 
- }
+}
+
+
+
+
