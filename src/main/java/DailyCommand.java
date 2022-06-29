d660159079e98f899c3f6ff7f94affbca96cf1c7
jalpp <92553013+jalpp@users.noreply.github.com>
2022-06-29 15:40:57 -0400

    fixed to new /dailypuzzle

--- src/main/java/DailyCommand.java
+++ src/main/java/DailyCommand.java
@@ -1,9 +1,6 @@
 import chariot.Client;
-import chariot.model.ExploreResult;
 import chariot.model.Puzzle;
 import chariot.model.Result;
-import com.github.bhlangonijr.chesslib.Board;
-import com.github.bhlangonijr.chesslib.move.MoveList;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import java.awt.*;
@@ -15,6 +12,7 @@ public class DailyCommand {
     private EmbedBuilder embedBuilder;
     private String gameID;
 
+
     public DailyCommand(Client client) {
         this.client = client;
         this.gameID = "";
@@ -23,6 +21,7 @@ public class DailyCommand {
 
 
     public EmbedBuilder getPuzzleData() {
+
 
         try {
 
@@ -34,40 +33,22 @@ public class DailyCommand {
 
                 this.gameID = puzzle1.game().id();
 
-
-
                 String puzzleID = puzzle1.puzzle().id();
                 String puzzleImg = "https://lichess1.org/training/export/gif/thumbnail/"  + puzzleID + ".gif";
-
-                List<String> solution = puzzle1.puzzle().solution();
-
-                String textMoves = "";
-
-                for(int i = 0; i < solution.size(); i++){
-                    textMoves += solution.get(i) + " ";
-                }
-
-
+                
 
                 Puzzle.PuzzleInfo puzzleInfo = puzzle1.puzzle();
 
-                String url = "https://lichess.org/training/daily";
-
                 int puzzleRating = puzzleInfo.rating();
-                List<String> themes = puzzleInfo.themes();
-
-                String the = "";
-
-                for (int i = 0; i < themes.size(); i++) {
-                    the += themes.get(i) + " \n ";
-                }
+            
+                
 
                 this.embedBuilder = new EmbedBuilder();
                 this.embedBuilder.setColor(Color.blue);
-                this.embedBuilder.setTitle("Daily Puzzle");
-                this.embedBuilder.setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/155/jigsaw-puzzle-piece_1f9e9.png");
+                this.embedBuilder.setTitle("\uD83E\uDDE9 Puzzle of The Day \uD83E\uDDE9");
                 this.embedBuilder.setImage(puzzleImg);
-                this.embedBuilder.setDescription("**Puzzle rating:** \n" + puzzleRating + "\n\n **Puzzle Themes:** \n " + the + "\n [Try the Puzzle](" + url + ")");
+                this.embedBuilder.setDescription("**" + "Puzzle Rating: " + puzzleRating+ "**");
+
 
               return this.embedBuilder;
             }
@@ -90,20 +71,7 @@ public class DailyCommand {
 
     }
 
+    
 
-    public String getPuzzleGame(){
-        Result<Puzzle> dailypuzzle = client.puzzles().dailyPuzzle();
-        this.gameID = dailypuzzle.get().game().id();
-        String puzzleGame = "";
-        puzzleGame += "https://lichess1.org/game/export/gif/" + this.gameID + ".gif";
-
-        return puzzleGame;
-    }
-
-
-
-
-
+    
 }
-
-
