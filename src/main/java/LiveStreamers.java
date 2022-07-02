0694dd56008c603c82f21d2850b441cc36900272
jalpp <92553013+jalpp@users.noreply.github.com>
2022-07-02 19:26:11 -0400

    added new streamer info

--- src/main/java/LiveStreamers.java
+++ src/main/java/LiveStreamers.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 
 import java.awt.*;
 import java.util.List;
+import java.util.Optional;
 
 public class LiveStreamers {
 
@@ -34,11 +35,32 @@ public class LiveStreamers {
                 title += "";
             }
 
-            getLivePeople +=" \uD83C\uDF99️ " + title +  " " +  live.get(i).id() + " is Streaming! \n " + " [**Profile**](https://lichess.org/@/" + live.get(i).id() + ") \n";
+          String presentlink = "";
+          String TwitchLink = "";
+          String YoutubeLink = "";
+
+            if(live.get(i).stream().service().equals("twitch")){
+               Optional<String> twitchLinks = live.get(i).streamer().twitch();
+               if(twitchLinks.isPresent()){
+                   TwitchLink = twitchLinks.get();
+                   presentlink += "[**Twitch** \uD83D\uDD2E ](" + TwitchLink + ") " + "**"+ live.get(i).streamer().headline() + "**"+ "\n\n";
+
+               }
+            }else{
+               Optional<String> youtubelinks = live.get(i).streamer().youTube();
+               if(youtubelinks.isPresent()){
+                   YoutubeLink = youtubelinks.get();
+                   presentlink += "[**Youtube** \uD83D\uDD3A ](" + YoutubeLink + ") " + "**"+ live.get(i).streamer().headline() + "**"+ "\n\n";
+               }
+            }
+
+
+
+            getLivePeople +=" \uD83C\uDF99️ " + title +  " **" +  live.get(i).id() + "**" + " is **Live**! \n " + presentlink;
 
 
         }
-        this.embedBuilder.setThumbnail("https://cdn5.vectorstock.com/i/1000x1000/14/24/twitch-and-youtube-logo-with-background-ima-vector-19461424.jpg");
+        this.embedBuilder.setThumbnail("https://media2.giphy.com/media/McsDYx2ihXzztTFMap/giphy.gif");
         this.embedBuilder.setTitle("Current Live Streamers: ");
         this.embedBuilder.setColor(Color.pink);
         this.embedBuilder.setDescription(getLivePeople);
