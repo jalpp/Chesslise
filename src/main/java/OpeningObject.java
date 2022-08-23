a0bea46b6ff87d2aedd35df85a04bc6bc555f75b
jalpp <92553013+jalpp@users.noreply.github.com>
2022-08-23 16:09:18 -0400

    Update OpeningObject.java

--- src/main/java/OpeningObject.java
+++ src/main/java/OpeningObject.java
@@ -1,24 +1,19 @@
-import net.dv8tion.jda.api.EmbedBuilder;
 
-import java.awt.*;
 
 public class OpeningObject {
 
-    private String name;
     private String fen;
 
-    public OpeningObject(String name, String fen){
-        this.name = name;
+    public OpeningObject(String fen){
+
         this.fen = fen;
     }
 
-    public EmbedBuilder getOpeningEmbed(){
-        EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setColor(Color.green);
-        embedBuilder.setTitle(this.name);
-        embedBuilder.setImage("https://chessboardimage.com/" + this.fen + ".png");
-        return embedBuilder;
+
+    public String getImage(){
+        return "https://chessboardimage.com/" + this.fen + ".png";
     }
+
 
 
 
