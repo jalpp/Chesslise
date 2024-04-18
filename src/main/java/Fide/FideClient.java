7c80d76c0a71dd2f1ddb30b448fda696c3c30ceb
jalpp <92553013+jalpp@users.noreply.github.com>
2024-04-17 22:42:25 -0400

    add docs and fix page size change

--- src/main/java/Fide/FideClient.java
+++ src/main/java/Fide/FideClient.java
@@ -11,10 +11,14 @@ import java.net.URL;
 
 
 // represents the FIDE API client for Java
+// Feel free to use the client in your codebase 
+// Full documentation for the API at http://fide.thanh.se/docs/index.html
+
 public class FideClient {
 
 
     private static final String API_ENDPOINT = "https://fide.thanh.se/api/players";
+
 
     public FideClient(){
 
@@ -26,12 +30,17 @@ public class FideClient {
        System.out.println(res);
     }
 
+    /**
+     * get the Top n (size) players by rating band of (bandName)
+     * size: the size of page
+     * bandName: the rating band
+     */
 
     public static String getTopNPlayerForNRatingBand(int size, String bandName){
         try {
 
-            //'fide.thanh.se/api/players?sort_by=standard&order=desc&size=10'
-            URL url = new URL(API_ENDPOINT + "?sort_by=" + bandName + "&order=desc&size=" + size);
+        
+            URL url = new URL(API_ENDPOINT + "?sort_by=" + bandName + "&order=desc&page_size=" + size);
 
 
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -67,11 +76,16 @@ public class FideClient {
 
     }
 
+    /**
+     * run a query on the fide api by providing query and searchTag
+     * query: query fields in FIDE API
+     * searchTag: the value to be searched for
+     */
+
 
     public static String getQueryByTag(String query, String searchTag){
         try {
 
-            //'fide.thanh.se/api/players?sort_by=standard&order=desc&size=10'
             URL url = new URL(API_ENDPOINT + "?" + query+ "=" + searchTag);
 
 
@@ -108,6 +122,11 @@ public class FideClient {
 
     }
 
+    /**
+     * creates a Top10 leaderboard String builder String, used for Discord app, can also be used for debugging
+     * responseData: A JSON FIDE data in string format
+     */
+
 
     public static String StringfyResponse(String responseData) {
         try {
@@ -130,6 +149,7 @@ public class FideClient {
         }
     }
 
+    // helper method used in the app and command /fide
 
     public static String getTopNInString(String ratingBand, int size){
         String res = getTopNPlayerForNRatingBand(size, ratingBand);
