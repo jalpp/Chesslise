a9ab194848af536eb73ab43ea6147750c90dedc4
jalpp <jalp.panchal44@gmail.com>
2025-07-10 20:55:21 +0000

    fix(pgnscroll): add image viewer

--- chesslise/src/main/java/abstraction/ChessUtil.java
+++ chesslise/src/main/java/abstraction/ChessUtil.java
@@ -2,6 +2,7 @@ package abstraction;
 
 import com.github.bhlangonijr.chesslib.Board;
 
+
 public class ChessUtil {
 
     private final Board board;
@@ -12,6 +13,7 @@ public class ChessUtil {
 
     }
 
+
     public String getImageFromFEN(String fen, String boardColor, String pieceType) {
         try {
             String img;
@@ -19,11 +21,9 @@ public class ChessUtil {
             String[] getImgCord = this.board.getFen().split(" ");
 
             if (fen.contains("w")) {
-                img = "https://lichess1.org/export/fen.gif?fen=" + getImgCord[0] + "&color=white&theme=" + boardColor
-                        + "&piece=" + pieceType;
+                img = "https://lichess1.org/export/fen.gif?fen=" + getImgCord[0] + "&color=white&theme=" + boardColor + "&piece=" + pieceType;
             } else {
-                img = "https://lichess1.org/export/fen.gif?fen=" + getImgCord[0] + "&color=black&theme=" + boardColor
-                        + "&piece=" + pieceType;
+                img = "https://lichess1.org/export/fen.gif?fen=" + getImgCord[0] + "&color=black&theme=" + boardColor + "&piece=" + pieceType;
             }
 
             return img;
@@ -33,16 +33,36 @@ public class ChessUtil {
         }
     }
 
-    public String getChessDBImage(String fen, String boardColor, String pieceType, String lastMove) {
+    public String getPgnView(String fen, String side,String boardColor, String pieceType){
+        try {
+            String img;
+            this.board.loadFromFen(fen);
+            String[] getImgCord = this.board.getFen().split(" ");
+
+            if (side.equalsIgnoreCase("white")) {
+                img = "https://lichess1.org/export/fen.gif?fen=" + getImgCord[0] + "&color=white&theme=" + boardColor + "&piece=" + pieceType;
+            } else {
+                img = "https://lichess1.org/export/fen.gif?fen=" + getImgCord[0] + "&color=black&theme=" + boardColor + "&piece=" + pieceType;
+            }
+
+            return img;
+
+        } catch (Exception e) {
+            return "Please provide a valid FEN!";
+        }
+    }
+
+
+    public String getChessDBImage(String fen, String boardColor, String pieceType, String lastMove){
         String img = getImageFromFEN(fen, boardColor, pieceType);
 
-        if (!img.equalsIgnoreCase("please provide a valid fen!")) {
+        if(!img.equalsIgnoreCase("please provide a valid fen!")){
             return img + "&lastMove=" + lastMove;
         }
 
         return img;
     }
-
+    
     public String getWhichSideToMove(String fen) {
         if (fen.contains("w")) {
             return "White to move";
@@ -51,8 +71,10 @@ public class ChessUtil {
         }
     }
 
+
     public String getAnalysisBoard(String fen) {
         return "https://lichess.org/analysis/standard/" + fen.replace(" ", "_");
     }
+
 
 }
